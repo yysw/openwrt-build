0fe2aec7ee1d36f0b281e47ba7e7d5d4f50ce705
Shengwu Yang <mailysw@gmail.com>
2026-08-06 19:04:13 +0800

    Clean up obsolete packages and dependencies
    Removed obsolete and unsupported components to prevent build failures.

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,3 +25,13 @@ find feeds/luci/ -type f -name "CMakeLists.txt" -exec sed -i 's/3.31/3.25/g' {} 
 # Remove obsolete SSR components that are no longer supported and causing SSL build failures
 rm -rf feeds/*/shadowsocksr-libev package/feeds/*/shadowsocksr-libev
 rm -rf feeds/*/luci-app-ssr-plus package/feeds/*/luci-app-ssr-plus
+
+# Remove luci-app-nikki due to missing required core components
+rm -rf feeds/passwall_dep/luci-app-nikki
+
+# Remove homeproxy and dockerman due to missing ucode module dependencies
+rm -rf feeds/luci/applications/luci-app-homeproxy
+rm -rf feeds/luci/applications/luci-app-dockerman
+
+# Remove HAProxy if advanced load balancing is not needed to avoid libcrypt-compat dependency issues
+rm -rf feeds/packages/net/haproxy
